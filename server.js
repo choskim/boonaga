f8fb0011d19f17f0e1d3f143272441492493ab44
Cho S. Kim <4cho.kim@gmail.com>
2014-06-30 16:31:37 -0700

    Adds server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var http   = require('http'),
+    server = null,
+    port   = 3000,
+    ip     = '127.0.0.1';
+
+var server = http.createServer(function(request, response) {
+  response.write("I'm alive!"); 
+  response.end();
+});
+
+server.listen(port, ip, function() {
+  console.log("Server is running on port %d", port);
+});
